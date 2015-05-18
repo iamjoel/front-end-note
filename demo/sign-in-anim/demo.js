12e38c4cc3594f366db9425512ec46d47d2b359e
金伟强 <iamjoel007@gmail.com>
2015-05-18 10:32:03 +0800

    enchance sign in random algorithm

--- demo/sign-in-anim/demo.js
+++ demo/sign-in-anim/demo.js
@@ -21,62 +21,62 @@ $(document).ready(function() {
         ROW: 8,
         COL: 8,
         // MAXNUM: 5,//最多出现人数
-        MAXNUM: 10, //最多出现人数
+        MAXNUM: 30, //最多出现人数
         DEFAULT_AVATAR_URL: 'img/avatar/default.png',
-        showItemLocArr: [],
+        availableLocs: [],// 存放未放签到的位置
+        notAvailableLocs: [],// 存放已放签到的位置
         isAnim: false,
         addArr: [],
-        makeRandomNoRepeatLoc: function() {
-            var loc = this.makeRandomLoc();
-            while (this.isInShowItem(loc)) {
-                loc = this.makeRandomLoc();
+        delayTime: 2000, // 新人签到的调用间隔
+        getRandomLoc: function() {
+            if(this.availableLocs.length === 0){
+                return false;
             }
-            return loc;
+            var randomIndex = Math.floor(Math.random() * this.availableLocs.length);
+            var loc = this.availableLocs.splice(randomIndex, 1)[0];
+            this.notAvailableLocs.push(loc);// loc 不在 availableLocs中，就在 notAvailableLocs中
+            // console.log('av:%d,notA:%d, total: %d',this.availableLocs.length, this.notAvailableLocs.length,  this.availableLocs.length +　this.notAvailableLocs.length);
+            return  loc;
         },
-        makeRandomLoc: function() {
-            return {
-                row: Math.floor(Math.random() * this.ROW),
-                col: Math.floor(Math.random() * this.COL)
-            };
-        },
-        isInShowItem: function(loc) {
-            var isIn = false;
-            this.showItemLocArr.forEach(function(each) {
-                if (each.row === loc.row && each.col === loc.col) {
-                    isIn = true;
-                    return false;
-                }
-            })
-            return isIn;
-        },
-        makeEmptyItem: function() {
-            return '<span class="sign-in__line__item"></span>';
+        removeRandomItem: function(done, itemURL) {
+            if(this.notAvailableLocs.length === 0){
+                return;
+            }
+            var loc = this.notAvailableLocs.splice(0, 1)[0];
+            var self = this;
+            $('#item-y-' + loc.y + '-x-' + loc.x).fadeOut(600, function() {
+                $(this).remove();
+                done.call(self, itemURL);
+                self.availableLocs.push(loc);
+            });
+
         },
         addItem: function(itemURL) {
-            if (this.showItemLocArr.length >= this.MAXNUM) {
-                this.removeRandomItem(this.makeAndAnimItem, itemURL);
-            } else {
-                this.makeAndAnimItem(itemURL);
-            }
+            this.makeAndAnimItem(itemURL);
+        },
+        removeItems: function (argument) {
+            var locArrs = this.notAvailableLocs.splice(0, this.notAvailableLocs.length - this.MAXNUM + 1);
+                locArrs.forEach(function(loc) {
+                    $('#item-y-' + loc.y + '-x-' + loc.x).remove();
+                });
+                this.availableLocs = this.availableLocs.concat(locArrs);
         },
         makeAndAnimItem: function(itemURL) {
             var self = this;
-            // fix 被调用太频繁时，人数会大于最大人数. 稍后还会继续加1个
-            if (this.showItemLocArr.length + 1 > this.MAXNUM) {
-                var locArrs = this.showItemLocArr.splice(0, this.showItemLocArr.length - this.MAXNUM + 1);
-                locArrs.forEach(function(loc) {
-                    $('#item-col-' + loc.col + '-row-' + loc.row).remove();
-                });
+            if (this.notAvailableLocs.length + 1 > this.MAXNUM) {
+                this.removeItems();
             }
             itemURL = itemURL || this.DEFAULT_AVATAR_URL;
-            var loc = this.makeRandomNoRepeatLoc();
-            this.showItemLocArr.push(loc);
-            console.log(JSON.stringify(loc));
+            var loc = this.getRandomLoc();
+            if(!loc){
+                return;
+            }
             var $img = $('<img>');
-            $img.attr('src', itemURL).attr('id', 'item-col-' + loc.col + '-row-' + loc.row);
+            $img.attr('src', itemURL).attr('id', 'item-y-' + loc.y + '-x-' + loc.x);
             $img.addClass('sign-in__img--anim');
             this.$el.append($img);
-            var $target = this.$rows.eq(loc.row).find('.sign-in__line__item').eq(loc.col);
+            var targetSel = '.row-{row}-col-{col}'.replace('{row}', loc.y).replace('{col}', loc.x);
+            var $target = $(targetSel);
             var pos = $target.position();
             $img.addClass('sign-in__img');
             $img.fadeIn('slow', function() {
@@ -84,8 +84,8 @@ $(document).ready(function() {
                 var offsetX = pos.left - imgPos.left + 'px';
                 var offsetY = pos.top - imgPos.top + 'px';
                 var translate3dStr = 'translate3d(' + offsetX + ',' + offsetY + ',0)';
-                var scaleX = self.itemWidth/$img.width();
-                var scaleY = self.itemHeight/$img.height();
+                var scaleX = self.itemWidth / $img.width();
+                var scaleY = self.itemHeight / $img.height();
                 var scaleStr = 'scale(' + scaleX + ',' + scaleY + ')';
                 setTimeout(function() {
                     $img.css({
@@ -96,38 +96,40 @@ $(document).ready(function() {
 
             this.$totalNum.text(++this.totalNum);
         },
-        removeRandomItem: function(done, itemURL) {
-            var loc = this.showItemLocArr.splice(0, 1)[0];
-            var self = this;
-            if (!loc) {
-                return;
-            }
-            $('#item-col-' + loc.col + '-row-' + loc.row).fadeOut(600, function() {
-                $(this).remove();
-                done.call(self, itemURL);
-            });
-
+        makeItem: function(row, col) {
+            return '<span class="sign-in__line__item row-{row}-col-{col}"></span>'.replace('{row}', row).replace('{col}', col);
         },
-        makeRow: function() {
+        makeRow: function(row) {
             var html = [];
             html.push('<div class="sign-in__line">');
             for (var i = 0; i < this.COL; i++) {
-                html.push(this.makeEmptyItem());
+                html.push(this.makeItem(row, i));
             }
             html.push('</div>');
             return html.join('');
         },
-        init: function() {
+        initAvailableLocs: function() { // 初始化可以放格子的位置 [{x:0,y:0}, {x:1,y:1}...]
+            for (var i = 0; i < this.ROW; i++) {
+                for (var j = 0; j < this.COL; j++) {
+                    this.availableLocs.push({x: j, y: i});
+                }
+            }
+        },
+        initView: function() {
             var html = [];
             for (var i = 0; i < this.ROW; i++) {
-                html.push(this.makeRow());
+                html.push(this.makeRow(i));
             }
             this.$el.html(html.join(''));
+        },
+        init: function() {
+            this.initView();
+            this.initAvailableLocs();
             this.$rows = this.$el.find('.sign-in__line');
             var $item = $('.sign-in__line__item');
             this.itemWidth = $item.width();
             this.itemHeight = $item.height();
-            this.addItem = queueCall(this.addItem, 2000, this); // 避免短时间调用太频繁
+            this.addItem = queueCall(this.addItem, this.delayTime, this); // 避免短时间调用太频繁而产生的动画卡顿
         }
     }
 
@@ -135,6 +137,12 @@ $(document).ready(function() {
 
     $('.add-btn').click(function() {
         signIn.addItem();
-    })
+    });
+
+    $('.add-multi-btn').click(function() {
+        for (var i = 40; i > 0; i--) {
+            signIn.addItem();
+        };
+    });
 
 });
