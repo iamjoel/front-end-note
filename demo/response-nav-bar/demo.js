c30d4a748f7640623f8dd4dad8173903f0f868f6
金伟强 <iamjoel007@gmail.com>
2016-08-25 17:31:12 +0800

    move to frontend demos

--- demo/response-nav-bar/demo.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function () {
-	var $navContents = $('.navbar__contents');
-	$('.navbar__menu-btn').click(function(){
-		$navContents.toggleClass('navbar__contents--show');
-	});
-});
